2b05bb39ad4f803efc952a1b2a9cdc537a408c40
Khushi Singh <khushi.singh3_cs.aiml24@gla.ac.in>
2025-09-02 20:57:51 +0530

    [khushi]bonus salary

---
+++ ControlFlow/Level1/Sumofnumber.java
@@ -0,0 +1,19 @@
+package Level1;
+import java.util.*;
+public class Sumofnumber {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i=n;
+        int sum=0;
+        while(i>=1){
+            sum=sum+i;
+            i--;
+            if(i<0){
+                break;
+            }
+        }
+                    System.out.println("sum is:"+sum);
+        sc.close();
+    }
+}
