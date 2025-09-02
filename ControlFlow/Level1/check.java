9bcaa004ec3b5ff83f74d82ff59549a9bc2269b1
Khushi Singh <khushi.singh3_cs.aiml24@gla.ac.in>
2025-09-02 20:25:29 +0530

    [khushi]largest

---
+++ ControlFlow/Level1/check.java
@@ -0,0 +1,20 @@
+package Level1;
+import java.util.*;
+public class check {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n =sc.nextInt();
+        if(n>0){
+            System.out.println("number is positive");
+        }
+        else if(n<0){
+            System.out.println("number is negative");
+        }
+        else{
+            System.out.println("number is 0");
+        }
+sc.close();
+
+    }
+}
