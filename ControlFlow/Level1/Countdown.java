c86112c4323daf970ea1f00a5ccca527c9fb303a
Khushi Singh <khushi.singh3_cs.aiml24@gla.ac.in>
2025-09-02 20:39:09 +0530

    [khushi]countdown

---
+++ ControlFlow/Level1/Countdown.java
@@ -0,0 +1,15 @@
+package Level1;
+import java.util.*;
+public class Countdown {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i=n;
+        while(i>=1){
+            System.out.println(i);
+            i--;
+        }
+        System.out.println("here is the countdown");
+        sc.close();
+    }
+}
