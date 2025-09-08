ba3931fbf49ab9e9a6493c7576ba902fd9b6039f
Khushi Singh <khushi.singh3_cs.aiml24@gla.ac.in>
2025-09-08 20:36:48 +0530

    [khushi]

---
+++ JavaString/Level2/LengthFinder.java
@@ -0,0 +1,29 @@
+package Level2;
+
+import java.util.Scanner;
+
+public class LengthFinder {
+    public static int findLength(String text) {
+        int count = 0;
+        try {
+            while (true) {
+                text.charAt(count); 
+                count++;            
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.next(); 
+        int manualLength = findLength(input);           
+        int builtInLength = input.length();           
+
+        System.out.println("Manual length:   " + manualLength);
+        System.out.println("Built-in length: " + builtInLength);
+        System.out.println("Are both equal?  " + (manualLength == builtInLength));
+    }
+}
